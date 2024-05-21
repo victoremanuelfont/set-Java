d836ecf2a6e6de0f7064484f75437804eae0f66a
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-05-21 08:32:59 -0300

    União, intersecção e diferença

---
+++ src/application/Program2.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Program2 {
+	public static void main(String[] args) {
+
+		Set<Integer> a = new TreeSet<>(Arrays.asList(0, 2, 4, 5, 6, 8, 10));
+		Set<Integer> b = new TreeSet<>(Arrays.asList(5, 6, 7, 8, 9, 10));
+
+		/*
+		 * 
+		 * "c" recebe "a", "c" é uma cópia de "a".
+		 * c.addAll(b) = União.
+		 */
+		Set<Integer> c = new TreeSet<>(a);
+		c.addAll(b);
+		System.out.println(c);
+
+		// intersection = somente os elementos que eles tem em comum
+		Set<Integer> d = new TreeSet<>(a);
+		d.retainAll(b);
+		System.out.println(d);
+
+		// difference = elementos do conjunto a que não existem no conjunto b.
+		Set<Integer> e = new TreeSet<>(a);
+		e.removeAll(b);
+		System.out.println(e);
+	}
+}
